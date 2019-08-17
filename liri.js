6a0972e0c987efbc16d5116ed5784be558c9be06
rivendell <webwyler@gmail.com>
2019-08-17 15:03:44 -0500

    finally got movie function to work so that no moviename given defaults to Mr Nobody

--- liri.js
+++ liri.js
@@ -2,17 +2,144 @@
 require('dotenv').config()
 
 // Using the require keyword lets us access all of the exports in the keys.js file
-let keys = require("./keys.js");
-console.log("keys: " + keys);
-console.log('my client ID is '  + keys.id);
-console.log('my client Secret is ' + keys.secret);
+var keys = require("./keys.js");
 
-let axios = require("axios");
-//console.log("axios: " + axios);
+var action = process.argv[2];
+//console.log("action is" + typeof action);
+var movieName = process.argv[3];
+//let value = process.argv[3];
 
-let moment = require("moment");
-//console.log("moment: " + moment);
+/* include axios npm package */
+var axios = require("axios");
 
-//let spotify = new Spotify(keys.Spotify);
+/* include moment npm package */
+var moment = require("moment");
 
-//console.log("Spotify says: " + spotify);
+// The switch-case will direct which function gets run.
+switch (action) {
+    case "movie-this":
+      movie();
+      break;
+    
+    case "concert-this":
+      concert();
+      break;
+    
+    case "spotify-this-song":
+      song();
+      break;
+    
+    case "do-what-it-says":
+      simon();
+      break;
+    }
+    
+    //If movie() is called...
+    function movie() {
+
+        /* if no movie title given.. display info for Mr Nobody */
+                
+        if (!movieName) {
+            var queryUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
+            //movieName = "Mr Nobody";            
+            //var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+            console.log(queryUrl);
+
+            axios
+            .get(queryUrl)
+            .then(function(response) {
+            
+            return console.log(`Title of movie: ${response.data.Title}
+            Year the movie came out: ${response.data.Year}
+            IMDB Rating of the movie: ${response.data.imdbRating} 
+            Rotten Tomatoes Rating of the movie: ${response.data.Ratings[1].Value}
+            Country where the movie was produced: ${response.data.Country} 
+            Language of the movie: ${response.data.Language}
+            Plot of the movie: ${response.data.Plot}
+            Actors in the movie: ${response.data.Actors}`);            
+        })
+        .catch(function(error) {
+            if (error.response)     {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+            } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+            } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+        } //end if statement
+        
+        else {
+            /* Then run a request with axios to the OMDB API with the movie specified */
+        //var movieName = process.argv[3];
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+        console.log(queryUrl);
+        console.log('!!movie name is: ' + movieName);
+
+        // console.log("moviename: " + movieName);
+
+         axios
+        .get(queryUrl)
+        .then(function(response) {            
+            console.log(`Title of movie: ${response.data.Title}
+            Year the movie came out: ${response.data.Year}
+            IMDB Rating of the movie: ${response.data.imdbRating} 
+            Rotten Tomatoes Rating of the movie: ${response.data.Ratings[1].Value}
+            Country where the movie was produced: ${response.data.Country} 
+            Language of the movie: ${response.data.Language}
+            Plot of the movie: ${response.data.Plot}
+            Actors in the movie: ${response.data.Actors}`);            
+        })
+        .catch(function(error) {
+            if (error.response)     {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+            } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+            } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+        } //end else statement
+    } //function end
+
+            
+
+
+
+        
+
+
+
+
+  
+
+
+
+  
+  
+
+
